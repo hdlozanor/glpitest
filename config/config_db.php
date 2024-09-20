65d6e942871c0705e9e5cc614230784e99c044d8
hdlozanor <85750264+hdlozanor@users.noreply.github.com>
2024-09-20 00:22:45 -0500

    Commit C1

--- config/config_db.php
+++ config/config_db.php
@@ -11,24 +11,10 @@ class DB extends DBmysql {
     public $allow_signed_keys = false;
 
     public function __construct() {
-        $this->dbhost = $_SERVER['dbhost'];
+        $this->dbhost = 'proyectos-instance-1.c8an9arwoaua.us-east-1.rds.amazonaws.com';
         $this->dbuser = $_SERVER['dbuser'];
         $this->dbpassword = $_SERVER['dbpassword'];
         $this->dbdefault = $_SERVER['dbdefault'];
-
-	error_log("DB Host: " . $this->dbhost);
-    	error_log("DB User: " . $this->dbuser);
-    	error_log("DB Password: " . $this->dbpassword);
-    	error_log("DB Name: " . $this->dbdefault);
-
-	try {
-        	$dsn = "mysql:host=$this->dbhost;dbname=$this->dbdefault;charset=utf8mb4";
-        	$this->pdo = new PDO($dsn, $this->dbuser, $this->dbpassword);
-        	$this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    	} catch (PDOException $e) {
-        	error_log("Connection failed: " . $e->getMessage());
-        	throw new Exception("Database connection error.");
-    	}
     }
 }
 ?>
