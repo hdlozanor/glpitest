6ccfccf1000590cd505fdcb8b3175692e0e2af63
hdlozanor <85750264+hdlozanor@users.noreply.github.com>
2024-09-20 01:18:19 -0500

    Commit 195

--- config/config_db.php
+++ config/config_db.php
@@ -15,5 +15,20 @@ class DB extends DBmysql {
         $this->dbuser = $_SERVER['dbuser'];
         $this->dbpassword = $_SERVER['dbpassword'];
         $this->dbdefault = $_SERVER['dbdefault'];
+
+	error_log("DB Host: " . $this->dbhost);
+    	error_log("DB User: " . $this->dbuser);
+    	error_log("DB Password: " . $this->dbpassword);
+    	error_log("DB Name: " . $this->dbdefault);
+
+	try {
+        	$dsn = "mysql:host=$this->dbhost;dbname=$this->dbdefault;charset=utf8mb4";
+        	$this->pdo = new PDO($dsn, $this->dbuser, $this->dbpassword);
+        	$this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    	} catch (PDOException $e) {
+        	error_log("Connection failed: " . $e->getMessage());
+        	throw new Exception("Database connection error.");
+    	}
     }
+}
 ?>
