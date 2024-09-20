b5e8b9b166b70ffd50a2bba46b350fc36d04c401
hdlozanor <85750264+hdlozanor@users.noreply.github.com>
2024-09-19 23:40:32 -0500

    Commit 777

--- config/config_db.php
+++ config/config_db.php
@@ -1,13 +1,20 @@
 <?php
 class DB extends DBmysql {
-   public $dbhost = $_SERVER['dbhost'];
-   public $dbuser = $_SERVER['dbuser'];
-   public $dbpassword = $_SERVER['dbpassword'];
-   public $dbdefault = $_SERVER['dbdefault'];
-   public $use_timezones = true;
-   public $use_utf8mb4 = true;
-   public $allow_myisam = false;
-   public $allow_datetime = false;
-   public $allow_signed_keys = false;
+    public $dbhost;
+    public $dbuser;
+    public $dbpassword;
+    public $dbdefault;
+    public $use_timezones = true;
+    public $use_utf8mb4 = true;
+    public $allow_myisam = false;
+    public $allow_datetime = false;
+    public $allow_signed_keys = false;
+
+    public function __construct() {
+        $this->dbhost = $_SERVER['dbhost'];
+        $this->dbuser = $_SERVER['dbuser'];
+        $this->dbpassword = $_SERVER['dbpassword'];
+        $this->dbdefault = $_SERVER['dbdefault'];
+    }
 }
 ?>
