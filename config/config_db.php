3e0e67c0beb49571ffb2202b0ae1e1e9f6f9b7cb
hdlozanor <hdlozanor@udistrital.edu.co>
2024-09-27 16:12:17 -0500

    Commit 123

--- config/config_db.php
+++ config/config_db.php
@@ -1,4 +1,9 @@
 <?php
+require 'aws.phar'; // Asegúrate de que la ruta sea correcta
+
+use Aws\SecretsManager\SecretsManagerClient;
+use Aws\Exception\AwsException;
+
 class DB extends DBmysql {
     public $dbhost;
     public $dbuser;
@@ -11,24 +16,38 @@ class DB extends DBmysql {
     public $allow_signed_keys = false;
 
     public function __construct() {
-        $this->dbhost = $_SERVER['dbhost'];
-        $this->dbuser = $_SERVER['dbuser'];
-        $this->dbpassword = $_SERVER['dbpassword'];
-        $this->dbdefault = $_SERVER['dbdefault'];
-
-	error_log("DB Host: " . $this->dbhost);
-    	error_log("DB User: " . $this->dbuser);
-    	error_log("DB Password: " . $this->dbpassword);
-    	error_log("DB Name: " . $this->dbdefault);
-
-	try {
-        	$dsn = "mysql:host=$this->dbhost;dbname=$this->dbdefault;charset=utf8mb4";
-        	$this->pdo = new PDO($dsn, $this->dbuser, $this->dbpassword);
-        	$this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    	} catch (PDOException $e) {
-        	error_log("Connection failed: " . $e->getMessage());
-        	throw new Exception("Database connection error.");
-    	}
+        $this->loadSecrets();
     }
+
+    private function loadSecrets() {
+        $client = new SecretsManagerClient([
+            'version' => 'latest',
+            'region' => 'us-east-1', // Por ejemplo, 'us-west-2'
+        ]);
+
+        try {
+            $result = $client->getSecretValue([
+                'SecretId' => 'MySQLProyectos', // ID del secreto en Secrets Manager
+            ]);
+
+            if (isset($result['SecretString'])) {
+                $secret = json_decode($result['SecretString'], true);
+                $this->dbhost = $secret['host'];
+                $this->dbuser = $secret['username'];
+                $this->dbpassword = $secret['password'];
+                $this->$dbdefault = $secret['dbdefault'];
+                echo "<script>
+                    console.log('DB Host: " . addslashes($this->dbhost) . "');
+                    console.log('DB User: " . addslashes($this->dbuser) . "');
+                    console.log('DB Password: " . addslashes($this->dbpassword) . "');
+                </script>";
+            }
+        } catch (AwsException $e) {
+            echo "Error al recuperar el secreto: " . $e->getMessage();
+        }
+    }
+
+    
 }
+
 ?>
