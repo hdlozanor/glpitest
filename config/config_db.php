8bf464a865d7e15a7c353f433fe34a72a33bb0d0
hdlozanor <hdlozanor@udistrital.edu.co>
2024-09-27 16:46:01 -0500

    Commit 777

--- config/config_db.php
+++ config/config_db.php
@@ -1,9 +1,4 @@
 <?php
-require '../lib/aws.phar'; // Asegúrate de que la ruta sea correcta
-
-use Aws\SecretsManager\SecretsManagerClient;
-use Aws\Exception\AwsException;
-
 class DB extends DBmysql {
     public $dbhost;
     public $dbuser;
@@ -16,38 +11,13 @@ class DB extends DBmysql {
     public $allow_signed_keys = false;
 
     public function __construct() {
-        $this->loadSecrets();
+        $this->dbhost = getenv('dbhost'); // valor por defecto
+        $this->dbuser = getenv('dbuser'); // valor por defecto
+        $this->dbpassword = getenv('dbpassword'); // valor por defecto
+        $this->dbdefault = getenv('dbdefault'); // valor por defecto
     }
-
-    private function loadSecrets() {
-        $client = new SecretsManagerClient([
-            'version' => 'latest',
-            'region' => 'us-east-1', // Por ejemplo, 'us-west-2'
-        ]);
-
-        try {
-            $result = $client->getSecretValue([
-                'SecretId' => 'MySQLProyectos', // ID del secreto en Secrets Manager
-            ]);
-
-            if (isset($result['SecretString'])) {
-                $secret = json_decode($result['SecretString'], true);
-                $this->dbhost = $secret['host'];
-                $this->dbuser = $secret['username'];
-                $this->dbpassword = $secret['password'];
-                $this->$dbdefault = $secret['dbdefault'];
-                echo "<script>
-                    console.log('DB Host: " . addslashes($this->dbhost) . "');
-                    console.log('DB User: " . addslashes($this->dbuser) . "');
-                    console.log('DB Password: " . addslashes($this->dbpassword) . "');
-                </script>";
-            }
-        } catch (AwsException $e) {
-            echo "Error al recuperar el secreto: " . $e->getMessage();
-        }
-    }
-
-    
 }
+    DB::initialize();
 
+    echo DB::$dbhost;
 ?>
